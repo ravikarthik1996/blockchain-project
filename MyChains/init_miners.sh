abd898228b1f3a0b707c5abb7fbecfc0c55fa043
Karthik <38332328+ravikarthik1996@users.noreply.github.com>
2020-06-06 13:03:14 -0400

    Delete init_miners.sh

--- MyChains/init_miners.sh
+++
@@ -1,9 +0,0 @@
-#!/bin/bash
-
-#Delete chaindata for miners
-rm -rf miner1/geth
-rm -rf miner2/geth
-
-#Initialize miners
-geth --datadir ./miner1 init genesis.json
-geth --datadir ./miner2 init genesis.json
